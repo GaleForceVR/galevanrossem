94bd7c924db41adda21298719cdab5dd0461414d
Gale Van Rossem <galeforcevr@gmail.com>
2015-01-10 01:41:47 -0600

    added basic styling with css

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,6 @@
 class PostController < ApplicationController
+  def show
+    @user = current_user if current_user
+    @post = Post.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :posts, only: [:show]
+  resources :post, only: [:show]
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
