1b728f535b31dddd6c027a339a2524e2d435a4f4
Gale Van Rossem <galeforcevr@gmail.com>
2015-01-11 13:35:23 -0600

    added tiny-mce without the gem

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,4 +3,28 @@ class PostController < ApplicationController
     @user = current_user if current_user
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if (@post.save)
+      redirect_to :action => :index
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def post_creation_params
+    params.require(:post).permit(:title, :content)
+  end
 end
