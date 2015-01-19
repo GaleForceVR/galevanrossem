d7e6356463b626163053b9a9044a8b9f1d84c875
Gale Van Rossem <galeforcevr@gmail.com>
2015-01-19 00:24:30 -0600

    added view for web portfolio

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def admin
   end
+  def web
+    @category = Category.find_by(name: "web")
+    @web_posts = @category.posts.order(:created_at) if @category
+  end
 end
