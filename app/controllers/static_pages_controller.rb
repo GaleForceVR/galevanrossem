f88a06ceedf2e7d344709c7ba5d5d7201dfb0ccd
Gale Van Rossem <galeforcevr@gmail.com>
2015-01-21 19:06:45 -0600

    added buttons/views and styling for portfolio pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,9 @@ class StaticPagesController < ApplicationController
     @category = Category.find_by(name: "web")
     @web_posts = @category.posts.order(:created_at) if @category
   end
+
+  def design
+    @category = Category.find_by(name: "design")
+    @design_posts = @category.posts.order(:created_at).reverse_order if @category
+  end
 end
