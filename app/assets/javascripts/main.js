af7d7cbb583011a5889f5f1a54ac5e14716d6949
Gale Van Rossem <galeforcevr@gmail.com>
2015-01-25 08:42:21 -0600

    fixed minor issues and added creddle.io resume page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,20 @@
 $(document).on('click', 'nav ul li', function() {
+  // e.preventDefault();
   $('.logo').css("z-index","999");
-  $('.menu-links').slideDown(100);
+  // $('.menu-icon').css("display","none");
+  // $('.menu-close-icon').css("display","block");
+  $('.menu-links').slideDown(200);
+  $('.social-button').show(800);
+  // $('.menu-links').show();
 });
+
+// $(document).on('click', '.menu-close-icon', function(e) {
+//   e.preventDefault();
+  
+//   $('.menu-links').stop().slideUp(100);
+//   $('.menu-icon').css("display","block");
+//   $('.menu-close-icon').css("display","none");
+// });
 
 $(document).on({
   mouseenter: function(){
@@ -12,6 +25,7 @@ $(document).on({
   },
   mouseleave: function(){
     $('.logo').css("z-index","4000");
-    $('.menu-links').stop().slideUp(100);
+    $('.social-button').hide(1);
+    $('.menu-links').stop().slideUp(400);
   }
 }, '.menu-links'); // passing the selector in as an argument to the .on function
